dfa663eec5053b5a4fc889c7adf8a0e375466ff1
jarokaz <jarokaz@no-reply.com>
2022-02-14 00:29:43 +0000

    wip

--- pipeline/run.py
+++ pipeline/run.py
@@ -48,14 +48,14 @@ def _main(argv):
 
     models = [
         {
-        'model_name': 'model_1', 'random_seed': 1,
+            'model_name': 'model_1', 'random_seed': 1,
         },
-               {
-        'model_name': 'model_2', 'random_seed': 2,
+        {
+            'model_name': 'model_2', 'random_seed': 2,
         },
-               {
-        'model_name': 'model_3', 'random_seed': 3,
-        },
+        #{
+        #    'model_name': 'model_3', 'random_seed': 3,
+        #},
 
     ]
     
