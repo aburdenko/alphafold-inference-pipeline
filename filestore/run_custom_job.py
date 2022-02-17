db4b9e38d1bf6b1568eb05bd973aa227762a0354
jarokaz <jarokaz@no-reply.com>
2022-02-17 14:28:30 +0000

    wip

--- filestore/run_custom_job.py
+++ filestore/run_custom_job.py
@@ -15,42 +15,94 @@
 
 import os
 import random
+import logging
 from re import I
 import sys
 import time
 from datetime import datetime
-
+import pprint
+import json
 
 from absl import flags
 from absl import app
 
 import google.cloud.aiplatform as aip
 
+from google.cloud import aiplatform
+from google.cloud.aiplatform_v1beta1 import types
+from google.cloud.aiplatform_v1beta1.services.job_service import JobServiceClient
+
+import google.auth
+
+
+
 
 _PIPELINE_JOB_NAME = 'alphafold-inference'
 
 FLAGS = flags.FLAGS
 
-flags.DEFINE_string('pipeline_spec', 'alphafold-inference.json', 'Path to pipeline spec')
-flags.DEFINE_string('pipeline_staging_location', 'gs://jk-vertex-staging/pipelines', 'Vertex AI staging bucket')
-flags.DEFINE_string('dsub_logging_path', 'gs://jk-dsub-staging/logging', 'dsub logging')
-flags.DEFINE_string('project', 'jk-mlops-dev', 'GCP Project')
-flags.DEFINE_string('region', 'us-central1', 'GCP Region')
-flags.DEFINE_string('fasta_path', 'gs://jk-alphafold-datasets-archive/fasta/T1044.fasta', 'A path to a sequence')
-flags.DEFINE_string('vertex_sa', 'training-sa@jk-mlops-dev.iam.gserviceaccount.com', 'Vertex SA')
-flags.DEFINE_string('pipelines_sa', 'pipelines-sa@jk-mlops-dev.iam.gserviceaccount.com', 'Pipelines SA')
-flags.DEFINE_string('uniref90_database_path', 'test1', 'Database paths')
-flags.DEFINE_string('databases_disk_image', 'http://test.com', 'Disk image prepopulated with databases')
-flags.DEFINE_string('max_template_date', '2020-05-14', 'Max template date')
-flags.DEFINE_integer('num_ensemble', 1, 'TBD')
-flags.DEFINE_integer('random_seed', None, 'TBD')
-flags.DEFINE_bool('enable_caching', False, 'Enable pipeline run caching')
-flags.DEFINE_bool('use_gpu_for_relaxation', True, 'Use GPU for relaxation')
-flags.DEFINE_string('sequence_desc', 'T1050 A7LXT1, Bacteroides Ovatus, 779 residues', '')
+flags.DEFINE_string('project', 'jk-mlops-dev', 'project')
+flags.DEFINE_string('project_number', '895222332033', 'project number')
+flags.DEFINE_string('region', 'us-central1', 'region')
+flags.DEFINE_string('nfs_server', '10.71.1.10', 'Filestore IP address')
+flags.DEFINE_string('nfs_root_path', '/datasets_v1', 'NFS fileshare')
+flags.DEFINE_string('mount_path', '/mnt/nfs/alphafold', 'NFS fileshare')
+flags.DEFINE_string('package_path', 'gs://jk-alphafold-staging/packages/mytrainingcode-0.1.tar.gz', 'alphafold staging')
+
+NFS_SERVER = '10.71.1.10'
+NFS_ROOT_PATH = '/datasets_v1' 
+MOUNT_PATH = '/mnt/nfs/alphafold' 
+IMAGE = 'gcr.io/jk-mlops-dev/nfs-test'
+SUBFOLDER='test'
+
+MASTER_SPEC = {
+  "machine_spec": {
+    "machine_type": "n1-highmem-2",
+  },
+  "replica_count": 1,
+  "nfs_mounts": [{
+    "server": NFS_SERVER,
+    "path": NFS_ROOT_PATH,
+    "mount_point": MOUNT_PATH,
+  }],
+  "container_spec": {
+    "image_uri": IMAGE,
+        "command": ["python"],
+        "args": [ "task.py",
+            f"--mount_root_path={MOUNT_PATH}",
+            f"--subfolder={SUBFOLDER}",
+        ],
+    },
+  }
+
+
 
 def _main(argv):
+ 
+    job_name = 'NFS_JOB_{}'.format(time.strftime("%Y%m%d_%H%M%S"))
 
-    #
+    credentials, _ = google.auth.default()
+    authed_session = google.auth.transport.requests.AuthorizedSession(credentials)
+    endpoint = f'https://{FLAGS.region}-aiplatform.googleapis.com/v1beta1/projects/{FLAGS.project}/locations/{FLAGS.region}/customJobs'
+    network = f'projects/{FLAGS.project_number}/global/networks/default' 
+
+    custom_job = {
+        "display_name": job_name,
+        "job_spec": {    
+            "worker_pool_specs": [
+                MASTER_SPEC,
+            ],
+            "network": network,
+        } 
+    }
+
+    pp=pprint.PrettyPrinter()
+    pp.pprint(custom_job)
+
+    response = authed_session.post(endpoint, data=json.dumps(custom_job))
+    print('*************************')
+    pp.pprint(response.json())
+
 
 
 if __name__ == "__main__":
---
+++ filestore/train_app/task.py
@@ -0,0 +1,55 @@
+import logging
+import os
+import sys
+import argparse
+
+from datetime import datetime
+
+MY_FILE_NAME='output_{}.txt'.format(datetime.now().strftime("%Y%m%d%H%M%S"))
+MY_FILE_CONTENT='Hello vertex AI'  #@param {type:"string"}
+
+def main(args):
+  # WRITE YOUR TRAINING CODE HERE
+  # For now we just write a file to the NFS and verify the file exists here
+  assert (os.path.exists(args.mount_root_path)),"Mounted Path Not Found"
+  print("Files in folder {} are:\n".format(args.mount_root_path))
+  print(os.listdir(args.mount_root_path))
+
+  output_dir = os.path.join(args.mount_root_path, args.subfolder)
+  if not os.path.exists(output_dir):
+    os.makedirs(output_dir)
+
+  print("Files in folder {} are:\n".format(output_dir))
+  print(os.listdir(output_dir))
+
+  output_file = os.path.join(
+      output_dir,
+      MY_FILE_NAME)
+  print("Writing a new file {} to the folder... \n".format(output_file))
+  f = open(output_file, "w")
+
+
+  f.write(MY_FILE_CONTENT)
+  f.close()
+
+  print("Files in folder {} are:\n".format(output_dir))
+  print(os.listdir(output_dir))
+
+  sys.exit(0)
+
+if __name__ == "__main__":
+  logging.getLogger().setLevel(logging.INFO)
+  PARSER = argparse.ArgumentParser()
+  # Input Arguments
+  PARSER.add_argument(
+      '--mount_root_path',
+      help='The path in worker where NFS is mount to',
+      default='/mnt/nfs/data')
+  PARSER.add_argument(
+      '--subfolder',
+      help='The subfolder under the mount path for writing files',
+      default='public')
+  
+  ARGUMENTS, _ = PARSER.parse_known_args()
+
+  main(ARGUMENTS)
